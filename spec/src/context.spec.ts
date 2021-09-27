bcf5ba0d9617b94dd3472aae897049b74d73d337
SalathielGenese <salathielgenese@gmail.com>
2021-09-27 13:48:03 +0400

    feat: test Context#inject() effect on factory

---
+++ spec/src/context.spec.ts
@@ -0,0 +1,28 @@
+import { Context } from "../../src/context";
+
+describe('Context', () => {
+    beforeEach(() => context = new Context());
+
+    let context: Context;
+
+    describe('#inject()', () => {
+        it('to call factory once with context', async () => {
+            const factorySpy = jasmine.createSpy('factorySpy');
+            const timelapse: Context.Token<number> = Symbol();
+
+            await context.provide([timelapse], factorySpy).inject(timelapse);
+
+            expect(factorySpy).toHaveBeenCalledOnceWith(context);
+        });
+
+        it('to call factory once even if called multiple times', async () => {
+            const factorySpy = jasmine.createSpy('factorySpy').and.returnValue(1);
+            const timelapse: Context.Token<number> = Symbol();
+
+            context.provide([timelapse], factorySpy);
+            await Promise.all([context.inject(timelapse), context.inject(timelapse)]);
+
+            expect(factorySpy).toHaveBeenCalledOnceWith(context);
+        });
+    });
+});
