deb5a16f5bb011849865d3c0207d2b227b70d6ce
Salathiel Genese <salathiel@genese.name>
2022-11-17 14:06:34 +0100

    feat: use native private members

--- src/context.ts
+++ src/context.ts
@@ -12,11 +12,11 @@ export class Context {
     static readonly FACTORY_FAILURE = 'FACTORY_FAILURE';
     static readonly UNKNOWN_KEYS = 'UNKNOWN_KEYS';
 
-    private readonly _error = (error: Error, name: string) => Object.assign(error, { name }) as Error;
-    private readonly _factories = new Map<Context.Key, (context: Context) => any>();
-    private readonly _pendingDependencies = new Map<Context.Key, Promise<any>>();
-    private readonly _dependencies = new Map<Context.Key, any>();
-    private readonly _parent?: Context;
+    readonly #error = (error: Error, name: string) => Object.assign(error, {name}) as Error;
+    readonly #factories = new Map<Context.Key, (context: Context) => any>();
+    readonly #pendingDependencies = new Map<Context.Key, Promise<any>>();
+    readonly #dependencies = new Map<Context.Key, any>();
+    #parent?: Context;
 
     /**
      *
@@ -28,7 +28,7 @@ export class Context {
     static from(parent: Context): Context {
         const context = new Context();
         // @ts-expect-error: Cannot assign to '_parent' because it is a read-only property.ts(2540)
-        context._parent = parent;
+        context.#parent = parent;
 
         return context;
     }
@@ -41,14 +41,14 @@ export class Context {
      * @param factories A spread function, to compute dependencies, in the same order as keys.
      * @returns A reference to the context on which the method was invoked.
      */
-    provide<KA extends readonly Context.Key[]>(keys: KA, ...factories: Context.Factories<KA>): this {
+    provide<KA extends readonly Context.Key[]>(keys: KA, ...factories: Context.Factories<KA>[]): this {
         for (let i = 0, l = keys.length; i < l; i++) {
             if (null === factories[i] && 'function' === typeof keys[i]) {
-                this._factories.set(keys[i]!, () => Reflect.construct(keys[i]! as any, [this]));
+                this.#factories.set(keys[i]!, () => Reflect.construct(keys[i]! as any, [this]));
                 continue;
             }
 
-            this._factories.set(keys[i]!, factories[i]!);
+            this.#factories.set(keys[i]!, factories[i]!);
         }
 
         return this;
@@ -62,16 +62,16 @@ export class Context {
      * @returns An promise that resolve with an array of dependencies, matching the order of provided keys
      */
     async inject<KA extends readonly Context.Key[]>(...keys: KA): Promise<Context.UnboxedKeys<KA>> {
-        const unknownKeys = keys.filter(key => !this._factories.has(key));
+        const unknownKeys = keys.filter(key => !this.#factories.has(key));
 
         if (unknownKeys.length) {
-            if (!this._parent) {
+            if (!this.#parent) {
                 const error = new Error(`Unknown keys (${unknownKeys.length}): ${unknownKeys.map(symbol => symbol.toString())}`);
-                throw this._error(error, Context.UNKNOWN_KEYS);
+                throw this.#error(error, Context.UNKNOWN_KEYS);
             } else {
                 const ownKeys = keys.filter(key => !unknownKeys.includes(key));
                 // NOTE: We don't use await. We want parents to throw soon as they have unknow keys
-                const fromParent = this._parent.inject(...unknownKeys);
+                const fromParent = this.#parent.inject(...unknownKeys);
                 const fromSelf = this.inject(...ownKeys);
 
                 return Promise.all([fromParent, fromSelf]).then(([fromParent, fromSelf]) => {
@@ -86,32 +86,32 @@ export class Context {
         }
 
         return Promise.all(keys.map(key => {
-            if (this._dependencies.has(key)) {
-                return this._dependencies.get(key);
-            } else if (this._pendingDependencies.has(key)) {
+            if (this.#dependencies.has(key)) {
+                return this.#dependencies.get(key);
+            } else if (this.#pendingDependencies.has(key)) {
                 // NOTE: No error handling, because promise not created here
-                return this._pendingDependencies.get(key);
+                return this.#pendingDependencies.get(key);
             } else {
                 try {
-                    let outcome = this._factories.get(key)?.(this);
+                    let outcome = this.#factories.get(key)?.(this);
 
                     if (outcome instanceof Promise) {
-                        this._pendingDependencies.set(key, outcome = outcome.then(value => {
-                            this._pendingDependencies.delete(key);
-                            this._dependencies.set(key, value);
+                        this.#pendingDependencies.set(key, outcome = outcome.then(value => {
+                            this.#pendingDependencies.delete(key);
+                            this.#dependencies.set(key, value);
                             return value;
                         }).catch(suppressed => {
                             const error = new Error(`Factory failure for key "${key.toString() as any}"`);
-                            throw Object.assign(this._error(error, Context.FACTORY_FAILURE), { suppressed });
+                            throw Object.assign(this.#error(error, Context.FACTORY_FAILURE), {suppressed});
                         }));
                     } else {
-                        this._dependencies.set(key, outcome);
+                        this.#dependencies.set(key, outcome);
                     }
 
                     return outcome;
                 } catch (suppressed) {
                     const error = new Error(`Factory failure for key "${key.toString() as any}"`);
-                    throw Object.assign(this._error(error, Context.FACTORY_FAILURE), { suppressed });
+                    throw Object.assign(this.#error(error, Context.FACTORY_FAILURE), {suppressed});
                 }
             }
         })) as any;
@@ -137,9 +137,9 @@ export namespace Context {
 
     export type Factories<KA extends readonly Context.Key[]> = {
         readonly [K in keyof KA]: KA[K] extends Token<infer I> ? Factory<I>
-        : KA[K] extends Constructor<infer I, [Context]> ? null | Factory<I>
-        : KA[K] extends Constructor<infer I> ? Factory<I>
-        : never;
+            : KA[K] extends Constructor<infer I, [Context]> ? null | Factory<I>
+                : KA[K] extends Constructor<infer I> ? Factory<I>
+                    : never;
     };
 
     export type Factory<I = any> = (context: Context) =>
