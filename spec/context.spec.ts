79aaaa9dca7e98810ed8bd4e3d4fb270ac885b9d
Salathiel Genese <salathiel@genese.name>
2022-11-21 00:31:46 +0100

    feat: baseline test case

--- spec/context.spec.ts
+++ spec/context.spec.ts
@@ -1,7 +1,23 @@
 import {Context} from '@squall.io/context';
+import createSpy = jasmine.createSpy;
 
-describe('noop', () => {
-    it('does nothing', () => {
-        expect(Context).not.toBeNull();
+describe('Context', () => {
+    describe('orphan', () => {
+        it('inject string token', () => {
+            const context = new Context();
+            const expected = Math.random().toString(36);
+            const factorySpy = createSpy('factorySpy', _ => expected).and.callThrough();
+
+            expect(context.provide('primary', 'a', factorySpy)).toBe(context);
+            expect(factorySpy).withContext('Factory evaluated eagerly.').toHaveBeenCalledOnceWith(context);
+            expect(context.inject('primary', 'a')).toBe(expected);
+            expect(factorySpy).withContext('Factory never evaluated.').toHaveBeenCalledOnceWith(context);
+            expect(context.inject('primary', 'a')).toBe(expected);
+            expect(factorySpy).withContext('Factory never eagerly.').toHaveBeenCalledOnceWith(context);
+            expect(() => context.inject('a')).toThrowMatching(thrown =>
+                // FIXME: Should be missing token
+                thrown instanceof Error && thrown.message.startsWith(Context.ERR_EMPTY_VALUE));
+            expect(factorySpy).withContext('Factory never eagerly.').toHaveBeenCalledOnceWith(context);
+        });
     });
 })
