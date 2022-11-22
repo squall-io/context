f01b95d34069557cb8ec17cfa8ab04412d29ecd9
Salathiel Genese <salathiel@genese.name>
2022-11-22 09:22:26 +0100

    feat: test Context.provide(token: string, qualifier)

--- spec/context.spec.ts
+++ spec/context.spec.ts
@@ -146,6 +146,142 @@ describe('Context', () => {
                 .has('HOST', 'okay')).toBeTrue();
         });
     });
+
+    describe('.provide', () => {
+        describe('(token: string, factory)', () => {
+            it('return a reference to invoked context', () => {
+                const contextOne = new Context();
+                expect(contextOne.provide('target', () => '')).toBe(contextOne);
+
+                const contextTwo = new Context(contextOne);
+                expect(contextTwo.provide('target', () => '')).toBe(contextTwo);
+
+                const contextThree = new Context(contextOne, contextTwo);
+                expect(contextThree.provide('target', () => '')).toBe(contextThree);
+            });
+
+            it('register token', () => {
+                expect(new Context()
+                    .provide('target', () => '')
+                    .hasOwn('target')).toBeTrue();
+            });
+
+            it('register token at invoked context', () => {
+                const parents = [new Context(), new Context()];
+                const context = new Context(...parents);
+
+                context.provide('target', () => '');
+
+                expect(context.hasOwn('target')).toBeTrue();
+                expect(parents.every(parent => !parent.hasOwn('target'))).toBeTrue();
+            });
+
+            it('invoke factory function', () => {
+                const factory = createSpy('stringFactorySpy').and.returnValue(false);
+                const context = new Context().provide('target', factory);
+
+                expect(factory).toHaveBeenCalledOnceWith(context);
+            });
+
+            it('invoke factory function WHEN context configuration.factory.lazyFunctionEvaluation === false', () => {
+                const factory = createSpy('stringFactorySpy').and.returnValue(false);
+                const context = new Context({
+                    factory: {
+                        lazyFunctionEvaluation: false,
+                    },
+                }).provide('target', factory);
+
+                expect(factory).toHaveBeenCalledOnceWith(context);
+            });
+
+            it('do not invoke factory function WHEN context configuration.factory.lazyFunctionEvaluation === true', () => {
+                const factory = createSpy('stringFactorySpy');
+                new Context({
+                    factory: {
+                        lazyFunctionEvaluation: true,
+                    },
+                }).provide('target', factory);
+
+                expect(factory).not.toHaveBeenCalled();
+            });
+
+            it('validate factory-returned value', () => {
+                expect(() => new Context()
+                    .provide('address', () => '')).not.toThrow();
+                expect(() => new Context()
+                    .provide('address', () => Promise.resolve(''))).not.toThrow();
+                expect(() => new Context()
+                    .provide('address', () => Promise.resolve(null))).not.toThrow();
+                expect(() => new Context()
+                    .provide('address', () => Promise.resolve(undefined))).not.toThrow();
+                expect(() => new Context()
+                    .provide('address', () => null)).toThrowMatching(
+                    thrown => thrown instanceof Error && thrown.message.startsWith(Context.ERR_EMPTY_VALUE));
+                expect(() => new Context()
+                    .provide('address', () => undefined)).toThrowMatching(
+                    thrown => thrown instanceof Error && thrown.message.startsWith(Context.ERR_EMPTY_VALUE));
+            });
+
+            it('validate factory-returned value WHEN configuration.factory.lazyValidation === false', () => {
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: false,
+                    },
+                }).provide('address', () => '')).not.toThrow();
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: false,
+                    },
+                }).provide('address', () => Promise.resolve(''))).not.toThrow();
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: false,
+                    },
+                }).provide('address', () => Promise.resolve(null))).not.toThrow();
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: false,
+                    },
+                }).provide('address', () => Promise.resolve(undefined))).not.toThrow();
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: false,
+                    },
+                }).provide('address', () => null)).toThrowMatching(
+                    thrown => thrown instanceof Error && thrown.message.startsWith(Context.ERR_EMPTY_VALUE));
+            });
+
+            it('do not validate WHEN configuration.factory.lazyValidation === true', () => {
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: true,
+                    }
+                }).provide('address', () => null)).not.toThrow();
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: true,
+                    }
+                }).provide('address', () => undefined)).not.toThrow();
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: true,
+                    }
+                }).provide('address', () => Promise.resolve(null))).not.toThrow();
+                expect(() => new Context({
+                    factory: {
+                        lazyValidation: true,
+                    }
+                }).provide('address', () => Promise.resolve(undefined))).not.toThrow();
+            });
+
+            it('prevent overriding token at the same context level', () => {
+                expect(() => new Context()
+                    .provide('address', () => 'Earth')
+                    .provide('address', () => 'Earth')).toThrowMatching(thrown =>
+                    thrown instanceof Error && thrown.message.startsWith(Context.ERR_DUPLICATE_FACTORY));
+            });
+        });
+    });
     describe('orphan', () => {
         it('inject string token', () => {
             const context = new Context();
