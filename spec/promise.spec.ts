59a37f6ccf7f682b89162b1332be314e07aec2e6
Salathiel Genese <salathiel@genese.name>
2023-01-30 13:40:51 +0100

    feat: test promise constructor

--- spec/promise.spec.ts
+++ spec/promise.spec.ts
@@ -1,8 +1,40 @@
-import {describe, it} from '@jest/globals';
+import {describe, expect, it, jest} from '@jest/globals';
+// @ts-ignore
+const [TestedPromise, NativePromise] = [Promise, Promise];
+const {fn} = jest;
 
 describe('Promise', () => {
     describe('constructor( executor )', () => {
-        it('noop', () => {
+        it('execute executor immediately', () => {
+            const resolver = fn();
+            new TestedPromise(resolver);
+            expect(resolver).toHaveBeenCalledTimes(1);
+        });
+
+        it('execute executor with two functions', () => {
+            let resolve, reject;
+            const resolver = fn((res, rej) => [resolve, reject] = [res, rej]);
+            new TestedPromise(resolver);
+            expect(reject).not.toBeUndefined();
+            expect(resolve).not.toBeUndefined();
+            expect(resolver).nthCalledWith(1, resolve, reject);
+        });
+
+        it('calling either function settles the promise', async () => {
+            await expect(new TestedPromise(resolve => resolve(1))).resolves.toBe(1);
+            await expect(new TestedPromise((_, reject) => reject(-1))).rejects.toBe(-1);
+        });
+
+        it('throw error when resolve is not a function', () => {
+            expect(() => new TestedPromise(null as any)).toThrowError((thrown: any) =>
+                thrown instanceof TypeError &&
+                expect(thrown.message).toBe('Promise resolver null is not a function') || true);
+            expect(() => new TestedPromise(undefined as any)).toThrowError((thrown: any) =>
+                thrown instanceof TypeError &&
+                expect(thrown.message).toBe('Promise resolver undefined is not a function') || true);
+            expect(() => new TestedPromise([] as any)).toThrowError((thrown: any) =>
+                thrown instanceof TypeError &&
+                expect(thrown.message).toBe('Promise resolver [object Array] is not a function') || true);
         });
     });
 
